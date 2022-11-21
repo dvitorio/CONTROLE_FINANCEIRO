d341132ea4639dfbb5c6f0cbcc6015c8031229fe
Daniel Vitorio <dvitorio2@gmail.com>
2022-11-21 11:27:01 -0300

    Mudança geral na estilização e componentes

--- js/index.js
+++ js/index.js
@@ -1,14 +1,27 @@
-const dataInput = document.getElementById('dataInput');
-const cover = document.getElementById('cover');
-const btnVehicleButton = document.getElementById('btnVehicleButton');
-const btnCloseDataInput = document.getElementById('btnCloseDataInput');
+const title = document.getElementById('title');
+const btnClose = document.getElementById('btnClose');
+const btnPersonal = document.getElementById('btnPersonal');
+const btnVehicular = document.getElementById('btnVehicular');
+const vehicularScreen = document.getElementById('vehicular');
+const buttons = document.getElementById('buttons');
+const btnSave = document.getElementById('btnSave');
 
-btnVehicleButton.onclick =()=>{
-     cover.classList.add('d-none');
-     dataInput.classList.remove('d-none');
+//Open modal vehicular and close menu
+btnVehicular.onclick = ()=>{
+     title.classList.add('d-none');
+     vehicularScreen.classList.remove('d-none');
+     buttons.classList.add('d-none');
 }
 
-btnCloseDataInput.onclick =()=>{
-     cover.classList.remove('d-none');
-     dataInput.classList.add('d-none');
+//Close modal vehicular and open menu
+btnClose.onclick = ()=>{
+     title.classList.remove('d-none');
+     vehicularScreen.classList.add('d-none');
+     buttons.classList.remove('d-none');
+}
+
+//Save datas and close modal vehicular 
+btnSave.onclick = ()=>{
+     vehicularScreen.classList.add('d-none');
+     buttons.classList.remove('d-none');
 }
