9bdd1ac1e5b0f1d53351888935c79d225050e469
Daniel Vitorio <dvitorio2@gmail.com>
2022-11-29 08:46:25 -0300

    Ajustes na função de salvar registro de dívida

--- js/expenses.js
+++ js/expenses.js
@@ -3,15 +3,45 @@ const backGroundImage = document.getElementById('backGroundImage');
 const backGroundTitle = document.getElementById('backGroundTitle');
 const btnCancel = document.getElementById('btnCancel');
 const btnCancelExpenseVehicular = document.getElementById('btnCancelExpenseVehicular');
+const btnCloseDebtForm = document.getElementById('btnCloseDebtForm');
 const btnConfirmExpense = document.getElementById('btnConfirmExpense');
 const btnPrintVehicularExpenses = document.getElementById('btnPrintVehicularExpenses');
+const btnQuit = document.getElementById('btnQuit');
+const btnSaveDebtForm = document.getElementById('btnSaveDebtForm');
 const btnSaveVehicularExpenses = document.getElementById('btnSaveVehicularExpenses');
 const btnVehicular = document.getElementById('btnVehicular');
 const buttons = document.getElementById('buttons');
 const confirmExpense = document.getElementById('confirmExpense');
+const confirmExpenseSuccess = document.getElementById('confirmExpenseSuccess');
+const creditorName = document.getElementById('creditorName');
+const debtQuit = document.getElementById('debtQuit');
 const selectOptions = document.getElementById('selectOptions');
+const statusDebt = document.getElementById('statusDebt');
 const vehicularScreen = document.getElementById('vehicular');
 
+
+//OPEN MODAL DEBT REGISTER
+btnQuit.onclick = ()=>{
+     debtQuit.classList.remove('d-none');
+     backGroundImage.classList.add('d-none');
+     backGroundTitle.classList.add('d-none');
+}
+
+//SAVE DEBT REGISTER AND CLOSE MODAL
+btnSaveDebtForm.onclick = ()=>{
+     debtQuit.classList.add('d-none');
+     confirmExpense.classList.remove('d-none');
+     confirmExpenseSuccess.classList.remove('d-none');
+     backGroundImage.classList.remove('d-none');
+     backGroundTitle.classList.remove('d-none');
+}
+
+//CLOSE MODAL DEBT REGISTER
+btnCloseDebtForm.onclick = ()=>{
+     debtQuit.classList.add('d-none');
+     backGroundImage.classList.remove('d-none');
+     backGroundTitle.classList.remove('d-none');
+}
 
 //OPEN MODAL VEHICULAR AND CLOSE MENU
 btnVehicular.onclick = ()=>{
@@ -55,6 +85,7 @@ btnConfirmExpense.onclick = ()=>{
 btnPrintVehicularExpenses.onclick = ()=>{
      vehicularScreen.print();
 }
+
 
 
 const btnCalcular = document.getElementById('btnCalcular');
