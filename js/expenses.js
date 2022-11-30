4a43a0efad899efbb94197c337fee1278f2f4d51
Daniel Vitorio <dvitorio2@gmail.com>
2022-11-30 09:56:06 -0300

    Criação dos modais de cadastro de dívida, busca e cálculo de dívida

--- js/expenses.js
+++ js/expenses.js
@@ -1,36 +1,76 @@
 //INICIALIZAÇÃO DE VARIÁVEIS
 const backGroundImage = document.getElementById('backGroundImage');
 const backGroundTitle = document.getElementById('backGroundTitle');
+const btnAmort = document.getElementById('btnAmort');
 const btnCancel = document.getElementById('btnCancel');
+const btnCancelDebt = document.getElementById('btnCancelDebt');
 const btnCancelExpenseVehicular = document.getElementById('btnCancelExpenseVehicular');
 const btnCloseConfirmExpenseSuccess = document.getElementById('btnCloseConfirmExpenseSuccess');
 const btnCloseDebtForm = document.getElementById('btnCloseDebtForm');
+const btnCloseSearchDebt = document.getElementById('btnCloseSearchDebt');
+const btnCloseSubMenu = document.getElementById('btnCloseSubMenu');
 const btnConfirmExpense = document.getElementById('btnConfirmExpense');
 const btnPrintVehicularExpenses = document.getElementById('btnPrintVehicularExpenses');
 const btnQuit = document.getElementById('btnQuit');
+const btnRegisterDebt = document.getElementById('btnRegisterDebt');
 const btnSaveDebtForm = document.getElementById('btnSaveDebtForm');
 const btnSaveVehicularExpenses = document.getElementById('btnSaveVehicularExpenses');
+const btnSearchCreditor = document.getElementById('btnSearchCreditor');
+const btnSolveDebt = document.getElementById('btnSolveDebt');
+const btnSubMenu = document.getElementById('btnSubMenu');
 const btnVehicular = document.getElementById('btnVehicular');
 const buttons = document.getElementById('buttons');
 const confirmExpense = document.getElementById('confirmExpense');
 const confirmExpenseSuccess = document.getElementById('confirmExpenseSuccess');
 const creditorName = document.getElementById('creditorName');
-const debtQuit = document.getElementById('debtQuit');
+const debtRegister = document.getElementById('debtRegister');
+const debtSearch = document.getElementById('debtSearch');
+const resultDebt = document.getElementById('resultDebt');
 const selectOptions = document.getElementById('selectOptions');
+const solveDebt = document.getElementById('solveDebt');
 const statusDebt = document.getElementById('statusDebt');
+const subMenu = document.getElementById('subMenu');
 const vehicularScreen = document.getElementById('vehicular');
 
 
-//OPEN MODAL DEBT REGISTER
-btnQuit.onclick = ()=>{
-     debtQuit.classList.remove('d-none');
+//OPEN SUBMENU DEBT
+btnSubMenu.onclick = ()=>{
+     subMenu.classList.remove('d-none');
      backGroundImage.classList.add('d-none');
      backGroundTitle.classList.add('d-none');
 }
 
+//OPEN MODAL SEARCH CREDITOR
+btnSearchCreditor.onclick = ()=>{
+     subMenu.classList.add('d-none');
+     debtSearch.classList.remove('d-none');
+}
+
+//CLOSE MODAL SEARCH CREDITOR
+btnCloseSearchDebt.onclick = ()=>{
+     subMenu.classList.remove('d-none');
+     debtSearch.classList.add('d-none');
+}
+
+//CLOSE SUBMENU DEBT
+btnCloseSubMenu.onclick = ()=>{
+     subMenu.classList.add('d-none');
+     backGroundImage.classList.remove('d-none');
+     backGroundTitle.classList.remove('d-none');
+}
+
+//OPEN MODAL DEBT REGISTER
+btnRegisterDebt.onclick = ()=>{
+     subMenu.classList.add('d-none');
+     debtRegister.classList.remove('d-none');
+     backGroundImage.classList.add('d-none');
+     backGroundTitle.classList.add('d-none');
+}
+
+
 //SAVE DEBT REGISTER AND CLOSE MODAL
 btnSaveDebtForm.onclick = ()=>{
-     debtQuit.classList.add('d-none');
+     debtRegister.classList.add('d-none');
      confirmExpense.classList.remove('d-none');
      btnConfirmExpense.onclick = ()=>{
           confirmExpenseSuccess.classList.remove('d-none');
@@ -47,10 +87,36 @@ btnSaveDebtForm.onclick = ()=>{
 
 //CLOSE MODAL DEBT REGISTER
 btnCloseDebtForm.onclick = ()=>{
-     debtQuit.classList.add('d-none');
+     debtRegister.classList.add('d-none');
      backGroundImage.classList.remove('d-none');
      backGroundTitle.classList.remove('d-none');
 }
+
+//CALCULATE AND SOLVE DEBT IN DEBT FORM
+btnAmort.onclick = ()=>{
+     let balanceValue = parseFloat(document.getElementById('balanceValue').value.replace(',','.'));
+     let parcValueBalance = parseFloat(document.getElementById('parcValueBalance').value.replace(',','.'));
+     let resultAmort = parseFloat(document.getElementById('resultAmort').value);
+          if(balanceValue > parcValueBalance){
+               resultAmort = balanceValue - parcValueBalance;
+               resultDebt.classList.remove('d-none');
+               document.getElementById('resultAmort').value = resultAmort.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+          }
+          else{
+               alert('Valor da parcela não pode ser maior que o valor da dívida!');
+          }
+}
+
+//CLOSE SOLVE DEBT FORM
+btnCancelDebt.onclick = ()=>{
+     solveDebt.classList.add('d-none');
+     backGroundImage.classList.remove('d-none');
+     backGroundTitle.classList.remove('d-none');
+}
+
+
+
+/**========================MÓDULO DESPESAS VEICULARES ============================== */
 
 //OPEN MODAL VEHICULAR AND CLOSE MENU
 btnVehicular.onclick = ()=>{
@@ -72,6 +138,8 @@ btnCancel.onclick = ()=>{
 btnSaveVehicularExpenses.onclick = ()=>{
      vehicularScreen.classList.add('d-none');
      buttons.classList.remove('d-none');
+     backGroundImage.classList.remove('d-none');
+     backGroundTitle.classList.remove('d-none');
 }
 
 //SAVE DATA OND VEHICULAR MODAL, REQUIRE CONFIRMATION AND REOPEN MENU AND PAGE TITLE
@@ -97,18 +165,9 @@ btnPrintVehicularExpenses.onclick = ()=>{
 
 
 
-const btnCalcular = document.getElementById('btnCalcular');
 
-btnCalcular.onclick = ()=>{
-     let debtValue = parseFloat(document.getElementById('debtValue').value);
-     let numParc = parseFloat(document.getElementById('numParc').value);
-     let parcValue = document.getElementById('parcValue').value;
-     let balance = document.getElementById('balance').value;
-     parcValue = (debtValue/numParc);
-     balance = debtValue - parcValue;
-     document.getElementById('balance').value = balance.toFixed(2);
-     document.getElementById('parcValue').value = parcValue.toFixed(2);
-}
+
+
  
 
 
